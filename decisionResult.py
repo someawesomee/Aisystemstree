fefbeaceeb46346a5cbc39491b631d5ceeafc805
Alexey Golovanov <38816723+someawesomee@users.noreply.github.com>
2023-11-01 23:46:15 +0300

    DecisionResult.py

--- decisionResult.py
+++ decisionResult.py
@@ -1,54 +1,10 @@
-class DecisionResult:
-    def __init__(self, value: str):
-        self.value = value
+from decision import Decision
 
-    def __str__(self) -> str:
-        return self.value
 
-    def __eq__(self, other: 'DecisionResult') -> bool:
-        return self.value == other.value
+class DecisionResult(Decision):
+    def __init__(self, result):
+        self.result = result
 
-    def __ne__(self, other: 'DecisionResult') -> bool:
-        return not self.__eq__(other)
-
-    def __lt__(self, other: 'DecisionResult') -> bool:
-        return self.value < other.value
-
-    def __le__(self, other: 'DecisionResult') -> bool:
-        return self.value <= other.value
-
-    def __ge__(self, other: 'DecisionResult') -> bool:
-        return self.value >= other.value
-
-    def __gt__(self, other: 'DecisionResult') -> bool:
-        return self.value > other.value
-
-    def __ge__(self, other: 'DecisionResult') -> bool:
-        return self.value >= other.value
-
-    def __add__(self, other: 'DecisionResult') -> 'DecisionResult':
-        return DecisionResult(self.value + other.value)
-
-    def __sub__(self, other: 'DecisionResult') -> 'DecisionResult':
-        return DecisionResult(self.value - other.value)
-
-    def __mul__(self, other: 'DecisionResult') -> 'DecisionResult':
-        return DecisionResult(self.value * other.value)
-
-    def __truediv__(self, other: 'DecisionResult') -> 'DecisionResult':
-        return DecisionResult(self.value / other.value)
-
-    def __radd__(self, other: 'DecisionResult') -> 'DecisionResult':
-        return DecisionResult(self.value + other.value)
-
-    def __rsub__(self, other: 'DecisionResult') -> 'DecisionResult':
-        return DecisionResult(self.value - other.value)
-
-    def __rmul__(self, other: 'DecisionResult') -> 'DecisionResult':
-        return DecisionResult(self.value * other.value)
-
-    def __rtruediv__(self, other: 'DecisionResult') -> 'DecisionResult':
-        return DecisionResult(self.value / other.value)
-
-    def __r__ (self, other: 'DecisionResult') -> bool:
-        return self.value == other.value
+    def evaluate(self, client):
+        get_result_text = "YES" if self.result else "NO"
+        print(f"\nУ вас проблемы с сердцем: {get_result_text}")
