b6139c9531aa283fba662d46e1abe50037d6b441
Alexey Golovanov <38816723+someawesomee@users.noreply.github.com>
2023-11-02 01:41:34 +0300

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from decision import Client
+
+
+client_data = Client()
+client_data.BloodPressure = input("Имеются ли проблемы с давлением (yes/no): ").strip().lower() == "true"
+client_data.ChestPain = input("Имеется ли боль в груди (yes/no): ").strip().lower() == "true"
+client_data.Years = int(input("Ваш возраст больше 65 (число): "))
+client_data.ecgResult = input("Проблемы с сердцем по результату экг (yes/no): ").strip().lower() == "true"
 
 from tree import DecisionTree
-from decision import Client
-client_data = Client()
+
 decision_tree = DecisionTree.main_decision_tree()
 decision_tree.evaluate(client_data)
