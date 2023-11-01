eb9d2f6c963dd2947302cd7f4cd8bd3550f6efa9
Alexey Golovanov <38816723+someawesomee@users.noreply.github.com>
2023-11-02 01:41:16 +0300

    decisionResult.py

--- decisionResult.py
+++ decisionResult.py
@@ -1,10 +1,9 @@
 from decision import Decision
 
-
 class DecisionResult(Decision):
     def __init__(self, result):
         self.result = result
 
     def evaluate(self, client):
-        get_result_text = "YES" if self.result else "NO"
-        print(f"\nУ вас проблемы с сердцем: {get_result_text}")
+        get_result_text = "true" if self.result else "false"
+        print(f"{self.title}: {get_result_text}")
