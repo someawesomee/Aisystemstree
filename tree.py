24dd46d01b916b4e996f42d270621ef3bebfad4f
Alexey Golovanov <38816723+someawesomee@users.noreply.github.com>
2023-11-01 23:46:33 +0300

    tree.py

--- tree.py
+++ tree.py
@@ -1,27 +1,40 @@
 from DecisionQuery import DecisionQuery
+from DecisionResult import DecisionResult
 
-def tree():
-    def ecg():
-        ecg = DecisionQuery()
-        ecg.title = "Heart Attack Risk"
-        ecg.test = lambda client: client.ecg
-        ecg.positive = DecisionResult()
-        ecg.positive.result = True
-        ecg.negative = DecisionResult()
-        ecg.negative.result  = age
 
-    def age():
-        age  = DecisionQuery()
-        age.title = "Age >= 65"
-        age.test = lambda client: client.age >= 65
-        age.positive = ChestPain()
-        age.negative = DecisionResult()
-        age.negative.result = False
-    def ChestPain():
-        ChestPain = DecisionQuery()
-        ChestPain.title = "Chest Pain True?"
-        ChestPain.test = lambda client: client.ChestPain = 1
-        ChestPain.positive = 
+class DecisionTree:
+    @staticmethod
+    def main_decision_tree():
+        # Решение 3
+        pressure = DecisionQuery(
+            "Имеются ли проблемы с давлением",
+            lambda client: client.BloodPressure == "yes",
+            DecisionResult(True),
+            DecisionResult(False)
+        )
 
-    def PressureProblems():
-        pass
+        # Решение 2
+        pain = DecisionQuery(
+            "Имеется ли боль в груди",
+            lambda client: client.ChestPain == "yes",
+            pressure,
+            DecisionResult(False)
+        )
+
+        # Решение 1
+        age = DecisionQuery(
+            "Ваш возраст больше 65",
+            lambda client: client.Years == "yes",
+            DecisionResult(False),
+            pain
+        )
+
+        # Решение 0
+        ecgProblems = DecisionQuery(
+            "Проблемы с сердцем по результату экг",
+            lambda client: client.ecgResult == "yes",
+            age,
+            DecisionResult(False)
+        )
+
+        return ecgProblems
